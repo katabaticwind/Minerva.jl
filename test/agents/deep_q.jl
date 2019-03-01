078fafeaf49d825b089e08aebdd9e85078485ad2
katabaticwind <fastman@fastmail.com>
2019-03-01 06:04:15 -0500

    adjusted dqn

--- test/agents/deep_q.jl
+++ test/agents/deep_q.jl
@@ -1,9 +1,14 @@
 using Minerva, Flux
 using Flux: mse
 
-Q = Chain(Dense(4, 2))
+max_memory = 1000
+batch_size = 32
+discount_rate = 0.00
+update_steps = 100
+
+Q = Chain(Dense(4, 16, relu), Dense(16, 2))
 loss(x, y) = mse(x, y)
-opt = ADAM(0.01)
-agent = DeepQAgent(Q, loss, opt, 10, 2, 0.01)
+opt = ADAM(0.0001)
+agent = DeepQAgent(Q, loss, opt, max_memory, batch_size, discount_rate, update_steps)
 env = CartPole()
 history = train!(agent, env, max_episodes = 10000)
