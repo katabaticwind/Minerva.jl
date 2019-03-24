6dc57a5a78175deb1ebc23192446264bd1665ae5
katabaticwind <fastman@fastmail.com>
2019-03-23 23:30:58 -0400

    Updated vanilla policy gradient.

--- src/agents/vanilla_pg.jl
+++ src/agents/vanilla_pg.jl
@@ -4,15 +4,15 @@ using Flux.Optimise: _update_params!
 using Statistics
 using Formatting
 using Random
-
-
-# TODO: system for saving snapshots of the agent that can be re-loaded
-# TODO: run_episode gets path to save gif to
-# TODO: passing kwargs through as dicts?
+using StatsBase
 
 
 """
 VanillaPG
+
+    # Notes
+    - VPG chooses a random action according to its policy function, π.
+        - Can learn a (nearly) deterministic policy if that is optimal.
 """
 mutable struct VanillaPG
     model  # model
@@ -21,18 +21,12 @@ mutable struct VanillaPG
     γ  # discount rate
 end
 
-function action(agent::VanillaPG, env::AbstractEnvironment, ϵ = 0.0)
-    if rand() < ϵ
-        return rand(env.action_space)  # explore
-    else
-        return argmax(agent.model(env.state))  # exploit
-    end
-end
+action(agent::VanillaPG, env::AbstractEnvironment) = sample(env.action_space, weights(agent.model(env.state)))
 
-function compute_loss(agent::VanillaPG, history, reward)
-    S = hcat([o.s for o in history]...)
-    a = [o.a for o in history]
-    return mean(reward .* agent.model(S)[CartesianIndex.(a, axes(S, 2))])  # assume model parameterizes **log** or policy???
+function compute_loss(agent::VanillaPG, states, actions, rewards)
+    states = hcat(states...)
+    index = CartesianIndex.(actions, axes(states, 2))
+    return sum(cumsum(rewards) .* log.(agent.model(states)[index]))
 end
 
 function update!(agent::VanillaPG, loss)
@@ -41,32 +35,36 @@ function update!(agent::VanillaPG, loss)
     _update_params!(agent.opt, Flux.params(agent.model))
 end
 
-function run_episode(agent::VanillaPG, env::AbstractEnvironment; ϵ = 0.05, rendered = false, verbose = false)
+function run_episode(agent::VanillaPG, env::AbstractEnvironment; rendered = false, verbose = false)
     total_reward = 0.0
     steps = 0
     s′, done = reset!(env)
     rendered && render(env)
-    history = []
+    states = []
+    actions = []
+    rewards = []
     while !done
-        a = action(agent, env, ϵ)
         s = deepcopy(s′)
+        a = action(agent, env)
+        push!(states, s)
+        push!(actions, a)
         s′, r, done, info = step!(env, a)
-        total_reward += r
-        push!(history, (s=s, a=a))
         steps += 1
+        total_reward += r
+        push!(rewards, agent.γ^steps * r)  # NOTE: **discounted** rewards
         verbose && @info "s = $s, a = $a, r = $r, s′ = $s′, done = $done"
         steps == env.maxsteps && break
     end
     verbose && @info "episode finished in $steps timesteps"
-    loss = compute_loss(agent, history, total_reward)
+    loss = compute_loss(agent, states, actions, rewards)
     return total_reward, steps, loss
 end
 
-function evaluate(agent::VanillaPG, env::AbstractEnvironment, n = 100; ϵ = 0.05, verbose = false, rendered = false)
+function evaluate(agent::VanillaPG, env::AbstractEnvironment, n = 100; verbose = false, rendered = false)
     env.rendered = rendered
     history = []
     @progress for i = 1:n
-        total_reward, steps, _ = run_episode(agent, env, ϵ = ϵ, verbose = verbose)
+        total_reward, steps, _ = run_episode(agent, env, verbose = verbose)
         push!(history, (total_reward = total_reward, steps = steps))
     end
     total_reward = mean([e.total_reward for e in history])
@@ -75,7 +73,7 @@ function evaluate(agent::VanillaPG, env::AbstractEnvironment, n = 100; ϵ = 0.05
     return total_reward, steps
 end
 
-function train!(agent::VanillaPG, env::AbstractEnvironment; max_episodes = Inf, eval_freq = 1, eval_episodes = 100, eval_ϵ = 0.05, ϵ_schedule = nothing, verbose = false, patience = 5)
+function train!(agent::VanillaPG, env::AbstractEnvironment; max_episodes = Inf, eval_freq = 1, eval_episodes = 100, verbose = false, patience = 5)
 
     @info "training agent..."
     recorder = ExperimentRecorder("./log-$(randstring(5))")
@@ -85,35 +83,31 @@ function train!(agent::VanillaPG, env::AbstractEnvironment; max_episodes = Inf, 
     batch_size = agent.batch_size
     batches = 0
     episodes = 0
-    ϵ = ϵ_schedule(0)
 
     while improving && episodes < max_episodes
 
         loss = Array{Flux.Tracker.TrackedReal{Float64}, 1}(undef, batch_size)
         for i = 1:batch_size
-
             # training episode
-            total_reward, steps, loss[i] = run_episode(agent, env, ϵ = ϵ)
+            total_reward, steps, loss[i] = run_episode(agent, env)
             episodes += 1
-            ϵ = ϵ_schedule(episodes)
             verbose && @info "<training status message>"
-            message = (episode=episodes, score=total_reward, steps=steps, epsilon=ϵ)
+            message = (episode=episodes, score=total_reward, steps=steps, epsilon=NaN)
             stamp!(recorder, message, :train)
-
         end
-        batches += 1
 
         # network update
         update!(agent, loss)
+        batches += 1
+
+        # agent.opt.eta = agent.opt.eta * 0.99
 
         # fitness evaluation
         if batches % eval_freq == 0
-            total_reward, steps = evaluate(agent, env, eval_episodes, ϵ = eval_ϵ)
-            # @info "<testing status message>"
-            @info "episode: $episodes, score: $total_reward"
-            message = (episode=episodes, score=total_reward, steps=steps, epsilon=eval_ϵ)
+            total_reward, steps = evaluate(agent, env, eval_episodes)
+            @info "episode: $episodes, score: $total_reward, η: $(agent.opt.eta)"
+            message = (episode=episodes, score=total_reward, steps=steps, epsilon=NaN)
             stamp!(recorder, message, :test)
-            # run_episode(agent, env, rendered = true)  # to *see* how it's doing...
 
             # snapshots
             # if score > current_best_score
@@ -124,8 +118,7 @@ function train!(agent::VanillaPG, env::AbstractEnvironment; max_episodes = Inf, 
             #     n_evals_since_best_score == patience && break
             # end
         end
-
     end
     verbose && @info "agent finished training in $episodes episodes"
-    # return history
+    return recorder
 end
--- test/agents/vanilla_pg.jl
+++ test/agents/vanilla_pg.jl
@@ -1,12 +1,25 @@
 using Minerva, Flux
 
+"""
+    Softmax()
+"""
+struct Softmax end
+
+(a::Softmax)(x::AbstractArray) = softmax(x)
+
+Base.show(io::IO, l::Softmax) = print(io, "Softmax()")
+
+
 batch_size = 32
 discount_rate = 1.00
 
-model = Chain(Dense(4, 64, relu), Dense(64, 64, relu), Dense(64, 2))
+model = Chain(
+    Dense(4, 64, leakyrelu),
+    Dense(64, 2),
+    Softmax()
+)
 opt = RMSProp(1e-3)
 agent = VanillaPG(model, opt, batch_size, discount_rate)
 env = CartPole()
-ϵ_schedule = StepDecay(1.0, 0.05, 0.1, 50)
-train!(agent, env, max_episodes = 1000, ϵ_schedule = ϵ_schedule)
+recorder = train!(agent, env, max_episodes = 1000)
 evaluate(agent, env, 1, rendered = true)
